28b9cb5fe6af6d87fdd61fcdb07c8579a1451d81
appak21 <zhadiko111@gmail.com>
2022-08-01 22:43:49 +0600

    forum update

--- ui/js/hashtag.js
+++ ui/js/hashtag.js
@@ -1,37 +1,52 @@
-let input, hashtagArray, container, t;
+let input, container, t;
 
 TagsJoiner = '#get-tags'
 allTags = document.querySelector(TagsJoiner)
 
 input = document.querySelector('#hashtags');
 container = document.querySelector('.tag-container');
-hashtagArray = [];
+let hashtagArray = [];
 
 input.addEventListener('keyup', () => {
-  hashtagArray = [];
-    if (event.which == 32 && input.value.length > 0) {
-      var text = document.createTextNode(input.value);
-      var p = document.createElement('p');
-      container.appendChild(p);
-      p.appendChild(text);
-      p.classList.add('tag');
-      hashtagArray.push(input.value)
+    if (event.which == 32 && input.value.trim().length > 0 ) {
+      var text, p
+      let newtags = input.value
+      newtags.split(' ').forEach(element => {
+        if (isValid(element)) {
+          text = document.createTextNode(element);
+          p = document.createElement('p');
+          container.appendChild(p);
+          p.appendChild(text);
+          p.classList.add('tag');
+          hashtagArray.push(element)
+        }
+      });
+
       input.value = '';
+
+      allTags.value = hashtagArray.join(' ');
       
       let deleteTags = document.querySelectorAll('.tag');
       
       for(let i = 0; i < deleteTags.length; i++) {
         deleteTags[i].addEventListener('click', () => {
           container.removeChild(deleteTags[i]);
+          let name = deleteTags[i].innerHTML
+          hashtagArray = hashtagArray.filter(function(value, index, arr){
+            return value != name
+          });
+          allTags.value = hashtagArray.join(' ');
         });
-      }
-      
-      let newTagArr = []
-      let remtags = deleteTags;
-      for(let i = 0; i < remtags.length; i++) {
-        newTagArr.push(remtags[i].innerHTML)
-      }
-      allTags.value = newTagArr.join(' ')
+      };
     }
 });
 
+function isValid(tag) {
+  if (tag=="" || hashtagArray.includes(tag)) {
+    return false
+  }
+  if (tag.length > 30 || hashtagArray.length > 50) {
+    return false
+  }
+  return true
+}
